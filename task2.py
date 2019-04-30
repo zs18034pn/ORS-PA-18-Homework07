bc44f0d75717ce5f487264776a6b115645398cea
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-04-30 14:41:21 +0200

    task2

---
+++ task2.py
@@ -0,0 +1,25 @@
+
+"""
+===================   TASK 2  ====================
+* Name: Recursive Sum
+*
+* Write a recursive function that will sum given
+* list of integer numbers.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
+
+# Write your function here
+
+
+
+def main():
+    # Test your function here
+    pass
+
+if __name__ == "__main__":
+main()
