061d74c9685eb3308f60b2e516bbef7c88f0cfbc
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-04-30 14:44:08 +0200

    task3

---
+++ task3.py
@@ -0,0 +1,25 @@
+"""
+===================   TASK 3  ====================
+* Name: Insertion Sort
+*
+* Write a function that will implement insertion sort
+* algorithm. Generate list of 1000 random integer
+* numbers and sort the list using your function.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
+
+# Write your function here
+
+
+
+def main():
+    # Test your function here
+    pass
+
+if __name__ == "__main__":
+main()
