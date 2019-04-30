b3e7acbc92c7b1af3857448b15a2a8e943a800cf
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-04-30 14:45:06 +0200

    task4

---
+++ task4.py
@@ -0,0 +1,30 @@
+"""
+===================   TASK 4  ====================
+* Name: Number of Appearances
+*
+* Write a function that will return which element
+* of integer list has the greatest number of
+* appearances in that list.
+* In case that multiple elements have the same
+* number of appearances return any.
+*
+* Note: You are not allowed to use built-in
+* functions.
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Use main() function to test your solution.
+===================================================
+"""
+
+# Write your function here
+
+
+
+def main():
+    # Test your function here
+    pass
+
+if __name__ == "__main__":
+main()
