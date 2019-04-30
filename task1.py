ec492daa515291d199ac1381332e1eb1ee427e6c
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-04-30 14:40:10 +0200

    task1

---
+++ task1.py
@@ -0,0 +1,19 @@
+"""
+===================   TASK 1   ====================
+* Name: Roll The Dice
+*
+* Write a script that will simulate rolling the
+* dice. The script should fetch the number of times
+* the dice should be "rolled" as user input.
+* At the end, the script should print how many times
+* each number appeared (1 - 6).
+*
+* Note: Please describe in details possible cases
+* in which your solution might not work.
+*
+* Note: You can use `rand` module to simulate dice
+* rolling.
+===================================================
+"""
+
+# Write your script here
