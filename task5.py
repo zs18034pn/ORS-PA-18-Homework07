480ece0a7ae016481efc896b452ad23ca73f06de
UDG-ER <36456104+UDG-ER@users.noreply.github.com>
2019-04-30 14:50:30 +0200

    task5

---
+++ task5.py
@@ -0,0 +1,35 @@
+"""
+===================   TASK 5  ====================
+* Create class Vehicle which represents any 
+* vehicle and carries general info about the 
+* vehicle like: company that built that vehicle, 
+* model of the vehicle, year of production, 
+* registration number, engine power and color.
+* Create method cost_of_registration() that will 
+* return how much will registration cost for that 
+* instance of vehicle.
+*
+* Use this formula:
+
+     - Production year fees: 100 EUR  if production year < 1990
+                             200 EUR  if production year < 2000
+                             300 EUR  if production year < 2010 
+                             400 EUR  if production year < 2020
+     - On production year fee add engine fee:   (engine power in kw * 2 EUR)
+*
+* In your main function create single instance of the
+* Vehicle class and demonstrate the use of
+* cost_of_registration method.
+===================================================
+"""
+
+# Write your class here
+
+
+
+def main():
+    # Test your function here
+    pass
+
+if __name__ == "__main__":
+    main()
